07c6a0d282cdd2dbec0309c80383250b3d9f76ef
Khalil Delacerna <kdelacerna@sfo.yearup.org>
2017-06-20 08:02:32 -0700

    first commit

---
+++ PythonFinal.py
@@ -0,0 +1,29 @@
+print ("* ------------- *")
+print ("*               *")
+print ("* Final Project *")
+print ("*               *")
+print ("* ------------- *")
+
+
+#Asks for name then displays it
+n = input('Please enter your name: ')
+
+if(n == ''):
+	print('Sorry but you must enter a name ')
+
+else:
+	print ('Hi ' + n)
+
+#Asks for a number then displays it
+x = int(input('Please enter a number: '))
+
+if(x ==	''):
+	print('Sorry but you must enter a number')
+
+else:
+	while(1 < x):
+		print(x)
+		x = x - 1
+
+print("1")
+
